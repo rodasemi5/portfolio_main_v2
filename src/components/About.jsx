520a0881bfb74db3c0552f5676120196c9ef71af
Rodrigo Seminario <rodasemi5@gmail.com>
2023-04-14 18:59:56 -0400

    updates to text in About/Work

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -56,11 +56,9 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm software developer with experience in JavaScript, SQL and TypeScript.
-        also skilled using frameworks such as React, Next.js, Node.js and Three.js
-        I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+        I'm a software developer with experience in JavaScript, SQL and TypeScript. I am also skilled using frameworks such as React, Next.js, Node.js and Three.js.
+        I'm a quick learner and collaborate closely with clients to create efficient, scalable, and user-friendly solutions that solve real-world problems. 
+        Let's work together to bring your ideas to life!
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
